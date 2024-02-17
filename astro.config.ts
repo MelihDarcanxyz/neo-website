58c1679fd72ce70d3e8299affd6069d85f3c599d
Melih DARCAN <57872471+MelihDarcanxyz@users.noreply.github.com>
2024-02-17 14:29:50 +0300

    feat: astro robots.txt / webmanifest (not working) integration

--- astro.config.ts
+++ astro.config.ts
@@ -9,63 +9,55 @@ import { remarkReadingTime } from "./src/utils/remark-reading-time";
 import icon from "astro-icon";
 import expressiveCode from "astro-expressive-code";
 import { expressiveCodeOptions } from "./src/site.config";
+import robotsTxt from "astro-robots-txt";
+// import webmanifest from "astro-webmanifest";
 
 // https://astro.build/config
 export default defineConfig({
-	// ! Please remember to replace the following site property with your own domain
-	site: "https://astro-cactus.chriswilliams.dev/",
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-		rehypePlugins: [
-			[
-				rehypeExternalLinks,
-				{
-					target: "_blank",
-					rel: ["nofollow, noopener, noreferrer"],
-				},
-			],
-		],
-		remarkRehype: {
-			footnoteLabelProperties: {
-				className: [""],
-			},
-		},
-	},
-	integrations: [
-		expressiveCode(expressiveCodeOptions),
-		icon(),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		sitemap(),
-		mdx(),
-	],
-	image: {
-		domains: ["webmention.io"],
-	},
-	// https://docs.astro.build/en/guides/prefetch/
-	prefetch: true,
-	vite: {
-		plugins: [rawFonts([".ttf", ".woff"])],
-		optimizeDeps: {
-			exclude: ["@resvg/resvg-js"],
-		},
-	},
+  // ! Please remember to replace the following site property with your own domain
+  site: "https://melihdarcan.xyz/",
+  markdown: {
+    remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: "_blank",
+      rel: ["nofollow, noopener, noreferrer"]
+    }]],
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: [""]
+      }
+    }
+  },
+  integrations: [expressiveCode(expressiveCodeOptions), icon(), tailwind({
+    applyBaseStyles: false
+  }), sitemap(), mdx(), robotsTxt()], //, webmanifest({
+  //   name: "Melih DARCAN's Blog",
+  // })],
+  image: {
+    domains: ["webmention.io"]
+  },
+  // https://docs.astro.build/en/guides/prefetch/
+  prefetch: true,
+  vite: {
+    plugins: [rawFonts([".ttf", ".woff"])],
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"]
+    }
+  }
 });
-
 function rawFonts(ext: Array<string>) {
-	return {
-		name: "vite-plugin-raw-fonts",
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore:next-line
-		transform(_, id) {
-			if (ext.some((e) => id.endsWith(e))) {
-				const buffer = fs.readFileSync(id);
-				return {
-					code: `export default ${JSON.stringify(buffer)}`,
-					map: null,
-				};
-			}
-		},
-	};
+  return {
+    name: "vite-plugin-raw-fonts",
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore:next-line
+    transform(_, id) {
+      if (ext.some(e => id.endsWith(e))) {
+        const buffer = fs.readFileSync(id);
+        return {
+          code: `export default ${JSON.stringify(buffer)}`,
+          map: null
+        };
+      }
+    }
+  };
 }
